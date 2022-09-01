9dc20e31c70b865c19fec53c5920f38b39afa35d
Luca Borzacchiello <lucaborza@gmail.com>
2022-09-01 13:30:47 +0200

    show multiple instructions in UI

--- src/main.c
+++ src/main.c
@@ -29,6 +29,10 @@ int main(int argc, char const* argv[])
     Window*   win = mk_game_window();
     window_present(win);
 
+    uint16_t old_pc       = 0;
+    char*    disas_vec[5] = {0};
+
+    int invalidate_disas_vec = 0;
     while (1) {
         if (window_poll_event(&e)) {
             if (e.type == SDL_QUIT) {
@@ -40,24 +44,50 @@ int main(int argc, char const* argv[])
                     int sl = sys->ppu->scanline;
                     while (sys->ppu->scanline == sl)
                         system_step(sys);
+
+                    invalidate_disas_vec = 1;
                 } else if (e.key.keysym.sym == SDLK_f) {
                     int f = sys->ppu->frame;
                     while (sys->ppu->frame == f)
                         system_step(sys);
+
+                    invalidate_disas_vec = 1;
                 } else if (e.key.keysym.sym == SDLK_q) {
                     break;
                 }
             }
         }
 
+        if (invalidate_disas_vec) {
+            invalidate_disas_vec = 0;
+            old_pc               = 0;
+            for (int i = 0; i < sizeof(disas_vec) / sizeof(char*); ++i) {
+                free(disas_vec[i]);
+                disas_vec[i] = NULL;
+            }
+        }
+
+        if (sys->cpu->PC != old_pc) {
+            old_pc = sys->cpu->PC;
+
+            free(disas_vec[0]);
+            for (int i = 0; i < sizeof(disas_vec) / sizeof(char*) - 1; ++i)
+                disas_vec[i] = disas_vec[i + 1];
+            disas_vec[sizeof(disas_vec) / sizeof(char*) - 1] =
+                strdup(cpu_disassemble(sys->cpu, sys->cpu->PC));
+        }
+
         window_prepare_redraw(win);
 
         int col_off = 256 * 2 / win->text_char_width + 1;
 
-        window_draw_text(win, 0, col_off, color_white,
-                         cpu_disassemble(sys->cpu, sys->cpu->PC));
-        window_draw_text(win, 3, col_off, color_white, cpu_tostring(sys->cpu));
-        window_draw_text(win, 10, col_off, color_white, ppu_tostring(sys->ppu));
+        for (int i = 0; i < sizeof(disas_vec) / sizeof(char*); ++i)
+            if (disas_vec[i])
+                window_draw_text(win, i + 1, col_off, color_white,
+                                 disas_vec[i]);
+
+        window_draw_text(win, 7, col_off, color_white, cpu_tostring(sys->cpu));
+        window_draw_text(win, 14, col_off, color_white, ppu_tostring(sys->ppu));
 
         window_present(win);
     }
