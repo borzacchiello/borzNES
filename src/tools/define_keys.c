ea7965a21e65917fe1305eaec46b864f6cd0467b
Luca Borzacchiello <lucaborza@gmail.com>
2022-10-09 12:14:19 +0200

    improved controller

--- src/tools/define_keys.c
+++ src/tools/define_keys.c
@@ -13,7 +13,7 @@ int main(int argc, char const* argv[])
 
     config_load(DEFAULT_CFG_NAME);
 
-    Window* win = window_build_for_text(4, 20);
+    Window* win = window_build_for_text(5, 32);
 
     SDL_Joystick** joysticks      = NULL;
     int            joysticks_size = SDL_NumJoysticks();
@@ -27,23 +27,25 @@ int main(int argc, char const* argv[])
     }
 
     char* keys[] = {
-        CFG_P1_A,      NULL, CFG_P1_B,     NULL, CFG_P1_START,  NULL,
-        CFG_P1_SELECT, NULL, CFG_P1_UP,    NULL, CFG_P1_DOWN,   NULL,
-        CFG_P1_LEFT,   NULL, CFG_P1_RIGHT, NULL, CFG_P2_A,      NULL,
-        CFG_P2_B,      NULL, CFG_P2_START, NULL, CFG_P2_SELECT, NULL,
-        CFG_P2_UP,     NULL, CFG_P2_DOWN,  NULL, CFG_P2_LEFT,   NULL,
-        CFG_P2_RIGHT,  NULL,
+        CFG_P1_A,     "P1 A",        CFG_P1_B,      "P1 B",      CFG_P1_START,
+        "P1 START",   CFG_P1_SELECT, "P1 SELECT",   CFG_P1_UP,   "P1 UP",
+        CFG_P1_DOWN,  "P1 DOWN",     CFG_P1_LEFT,   "P1 LEFT",   CFG_P1_RIGHT,
+        "P1 RIGHT",   CFG_P2_A,      "P2 A",        CFG_P2_B,    "P2 B",
+        CFG_P2_START, "P2 START",    CFG_P2_SELECT, "P2 SELECT", CFG_P2_UP,
+        "P2 UP",      CFG_P2_DOWN,   "P2 DOWN",     CFG_P2_LEFT, "P2 LEFT",
+        CFG_P2_RIGHT, "P2 RIGHT",
     };
 
     int i = 0;
-    window_draw_text(win, 0, 0, color_white, "choose key for:");
-    window_draw_text(win, 1, 0, color_white, keys[i]);
+    window_draw_text(win, 1, 1, color_white, "choose key for (q to quit):");
+    window_draw_text(win, 3, 4, color_white, keys[i + 1]);
     window_present(win);
 
     SDL_Event e;
     while (1) {
         if (window_poll_event(&e)) {
-            if (e.type == SDL_QUIT)
+            if (e.type == SDL_QUIT ||
+                (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_q))
                 break;
             uint64_t kval = input_handler_encode_button(e);
             if (kval == 0)
@@ -52,13 +54,13 @@ int main(int argc, char const* argv[])
             // info("Chosen %016x for %s", kval, keys[i]);
             config_set_value(keys[i], kval);
 
-            keys[i + 1] = (char*)kval;
             i += 2;
             if (i >= sizeof(keys) / sizeof(char*))
                 break;
             window_prepare_redraw(win);
-            window_draw_text(win, 0, 0, color_white, "choose key for:");
-            window_draw_text(win, 1, 0, color_white, keys[i]);
+            window_draw_text(win, 1, 1, color_white,
+                             "choose key for (q to quit):");
+            window_draw_text(win, 3, 4, color_white, keys[i + 1]);
             window_present(win);
         }
     }
