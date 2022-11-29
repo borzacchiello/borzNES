f810fd3097c4e47f586a5ee502a3696e1cad670c
Luca Borzacchiello <lucaborza@gmail.com>
2022-11-29 21:45:13 +0100

    load_state_fuzz_test: use an in-line version of system_load_state
    In this way we can close the input file in case of errors.

--- src/tests/load_state_fuzz_test.c
+++ src/tests/load_state_fuzz_test.c
@@ -7,6 +7,20 @@
 
 #define N 1000L
 
+static void load_state(System* sys, FILE* fin)
+{
+    Buffer ram_state = read_buffer(fin);
+    if (ram_state.size != sizeof(sys->RAM))
+        panic("system_load_state(): invalid buffer");
+    memcpy(sys->RAM, ram_state.buffer, ram_state.size);
+    free_or_fail(ram_state.buffer);
+
+    cartridge_deserialize(sys->cart, fin);
+    cpu_deserialize(sys->cpu, fin);
+    ppu_deserialize(sys->ppu, fin);
+    mapper_deserialize(sys->mapper, fin);
+}
+
 FUZZ_TEST_SETUP()
 {
     // Perform any one-time setup required by the FUZZ_TEST function.
@@ -15,11 +29,14 @@ FUZZ_TEST_SETUP()
 FUZZ_TEST(const uint8_t* data, size_t size)
 {
     System* sys = NULL;
+    FILE*   fin = NULL;
 
     int result = setjmp(env);
     if (result != 0) {
         if (sys)
             del_sys(sys);
+        if (fin)
+            fclose(fin);
         return;
     }
 
@@ -28,14 +45,20 @@ FUZZ_TEST(const uint8_t* data, size_t size)
 
     // FIXME: load the state from buffer... This is super slow
     static const char* fname = "/dev/shm/input.state";
-    FILE* fout = fopen(fname, "wb");
+    FILE*              fout  = fopen(fname, "wb");
     if (fout == NULL)
         abort();
     if (fwrite(data, 1, size, fout) != size)
         abort();
     if (fclose(fout) != 0)
         abort();
-    system_load_state(sys, fname);
+
+    fin = fopen(fname, "rb");
+    if (fin == NULL)
+        abort();
+    load_state(sys, fin);
+    fclose(fin);
+    fin = NULL;
 
     for (long i = 0; i < N; ++i) {
         uint64_t cpu_cycles = cpu_step(sys->cpu);
